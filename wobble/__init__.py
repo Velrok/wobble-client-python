510e4b93f6b0a7b73bc12d60dc90e8315674967c
Uepsilon <Maxim.Kuepper@gmail.com>
2013-03-13 21:35:46 +0100

    added all methods for api

--- wobble/__init__.py
+++ wobble/__init__.py
@@ -1,56 +1,130 @@
+# -*- coding: utf-8 -*-
+
+
+def requires_login(fn):
+    def wrapper(*args):
+        # TODO: check if logged in, else throw error
+
+        # call method
+        fn(*args)
+    return wrapper
+
+
 class WobbleService(object):
     """WobbleService"""
-    def __init__(self, api_key, api_endpoint="http://wobble.moinz.de/endpoint.php"):
+    def __init__(self, api_endpoint):
         super(WobbleService, self).__init__()
-        self.api_key = api_key
+
         self.api_endpoint = api_endpoint
 
-    def check_version(self):
+    def wobble_api_version(self):
+        # method_name = 'wobble.api_version'
         pass
 
-    def list_inbox(self):
-        "Returns a list of all topics in the users inbox."
+    @requires_login
+    def topics_list(self, archived=False):
         pass
 
-    def list_archive(self):
-        "Returns a list of all topics in the users inbox."
+    @requires_login
+    def topics_search(self, query):
         pass
 
-    def list_unread_topics(self):
+    @requires_login
+    def topics_create(self, ):
         pass
 
-    def list_contacts(self):
+    @requires_login
+    def topic_get_details(self, topic_id):
+        # params => id
         pass
 
-    def list_comment_authors(self, topic_id, comment_id):
+    @requires_login
+    def topic_add_user(self, topic_id, contact_id):
         pass
 
-    def list_comments(self, topic_id):
+    @requires_login
+    def topic_remove_user(self, topic_id, contact_id):
+
         pass
 
-    def add_contact(self):
+    @requires_login
+    def topic_set_archived(self, topic_id, archived):
+        # archived => Boolean(0,1)
         pass
 
-    def add_participant_to_topic(self, participant_id, topic_id):
+    @requires_login
+    def topic_remove_message(self, topic_id, post_id):
+        # params => message_id
         pass
 
-    def search(self, query):
+    @requires_login
+    def post_create(self, topic_id, post_id, parent_post_id, intended_reply):
+        # intended_reply => Boolean(0,1)
         pass
 
-    def archive_topic(self, topic_id):
+    @requires_login
+    def post_edit(self, topic_id, post_id, content, revision_no):
+        # revision_no => Number of current revision
         pass
 
-    def create_topic(self, content):
+    @requires_login
+    def post_delete(self, topic_id, post_id):
         pass
 
-    def fetch_topic_content(self, topic_id):
+    @requires_login
+    def post_read(self, topic_id, post_id, read):
+        # read => Boolean
         pass
 
-    def fetch_topic_participants(self, topic_id):
+    @requires_login
+    def post_change_lock(self, topic_id, post_id, lock):
+        # lock => Boolean(0,1)
         pass
 
-    def create_comment(self, topic_id, content):
+    @requires_login
+    def user_get(self):
+
         pass
 
-    def replay_to_comment(self, topic_id, comment_id, content):
+    @requires_login
+    def user_get_id(self):
+
+        pass
+
+    @requires_login
+    def user_register(self, email, password):
+
+        pass
+
+    @requires_login
+    def user_change_name(self, new_name):
+
+        pass
+
+    @requires_login
+    def user_change_password(self, new_password):
+        pass
+
+    @requires_login
+    def user_login(self, email, password):
+        pass
+
+    @requires_login
+    def user_signout(self):
+        pass
+
+    @requires_login
+    def get_notifications(self, next_timestamp=None):
+        pass
+
+    @requires_login
+    def user_get_contacts(self):
+        pass
+
+    @requires_login
+    def user_add_contact(self, contact_email):
+        pass
+
+    @requires_login
+    def user_remove_contact(self, contact_id):
         pass
