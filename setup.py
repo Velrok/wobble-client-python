8a2a1a53162033ff9f9738c0c2c43c4cace19958
Waldemar Schwan <waldemar.schwan@adcloud.com>
2013-04-06 16:37:03 +0200

    experimental setup.py for pip installability

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wobble-client-python',
+      version='0.1.0',
+      description='A pyhton client for the wobble api.',
+      url='https://github.com/Velrok/wobble-client-python.git',
+      author='Velrok',
+      author_email='waldemar.schwan@gmail.com',
+      license='MIT',
+      packages=['wobble'],
+      zip_safe=False)
